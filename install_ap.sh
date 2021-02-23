23149ded2b656c9744301c8c76c92afbbcb99575
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-02-23 14:05:02 +0100

    Update install_ap.sh

--- install_ap.sh
+++ install_ap.sh
@@ -1,6 +1,6 @@
-echo "Installing ap"
-apt-get install hostapd wpasupplicant dnsmasq
-update-rc.d -f hostapd remove
 update-rc.d -f dnsmasq remove
 cat ./templates/interface.template >> ~/etc/network/interfaces
 
+sudo raspi-config nonint do_boot_wait 0
+cat ./templates/hostapd.template >> ~/etc/default/hostapd
+cat ./templates/hostapd.conf.template >> ~/etc/hostapd/hostapd/hostapd.conf
