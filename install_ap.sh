c3e5c6c8af9a1754fe5baa8d4c20ab3d3a548603
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-02-23 12:18:38 +0100

    Update install_ap.sh

--- install_ap.sh
+++ install_ap.sh
@@ -3,4 +3,4 @@ apt-get install hostapd wpasupplicant dnsmasq
 update-rc.d -f hostapd remove
 update-rc.d -f dnsmasq remove
 cat ./templates/interface.template >> ~/etc/network/interfaces
-
+echo "Neue Zeile" >> test.txt
